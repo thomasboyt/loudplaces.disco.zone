d11972f84450655959f6406003ed3ebb2ef48422
Thomas Boyt <thomas@ledgerx.com>
2015-08-20 16:32:15 -0400

    Add charset to hopefully prevent further encoding issues

--- app/components/layouts/Page.js
+++ app/components/layouts/Page.js
@@ -12,18 +12,17 @@ const Page = React.createClass({
           {/* Used for page renders; client renders use <DocumentTitle />*/}
           <title>{this.props.pageTitle}</title>
 
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
           <link href="http://fonts.googleapis.com/css?family=Dosis:400,500,600,700" rel="stylesheet" type="text/css" />
           <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet" type="text/css" />
-
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 
           <link href="/assets/app.css" rel="stylesheet" />
 
           <script src="/assets/vendor.bundle.js" />
-
           <script src="https://www.youtube.com/iframe_api" />
-
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         </head>
 
         <body>
