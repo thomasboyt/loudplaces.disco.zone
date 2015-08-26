73b4410a7e497581c02a1491275be50831538102
Thomas Boyt <thomas@ledgerx.com>
2015-08-26 19:30:28 -0400

    Minimize CSS in optimize mode

--- webpack.config.js
+++ webpack.config.js
@@ -9,43 +9,53 @@ import autoprefixer from 'autoprefixer';
 import precss from 'precss';
 import postcssImport from 'postcss-import';
 
-export default {
-  entry: {
-    vendor: [
-      'moment',
-      'query-string',
-      'r-dom',
-      'remarkable',
-      'remarkable-jsx',
-      'timeout-transition-group'
-    ]
-  },
+export default function generateConfig(optimize) {
+  let cssLoaderString = 'css-loader!postcss-loader';
 
-  plugins: [
-    new ExtractTextPlugin('[name].css'),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  ],
-
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
-      }
-    ]
-  },
-
-  postcss: function () {
-    const watchCfg = {
-      onImport: function(files) {
-        files.forEach(this.addDependency);
-      }.bind(this)
-    };
-
-    return [
-      postcssImport(watchCfg),
-      autoprefixer,
-      precss
-    ];
+  if (optimize) {
+    cssLoaderString = 'css-loader?minimize=1!postcss-loader';
   }
+
+  let config = {
+    entry: {
+      vendor: [
+        'moment',
+        'query-string',
+        'r-dom',
+        'remarkable',
+        'remarkable-jsx',
+        'timeout-transition-group'
+      ]
+    },
+
+    plugins: [
+      new ExtractTextPlugin('[name].css'),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    ],
+
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style-loader', cssLoaderString)
+        }
+      ]
+    },
+
+    postcss: function () {
+      const watchCfg = {
+        onImport: function(files) {
+          files.forEach(this.addDependency);
+        }.bind(this)
+      };
+
+      return [
+        postcssImport(watchCfg),
+        autoprefixer,
+        precss
+      ];
+    }
+  };
+
+  return config;
 };
